73981ecd3d9d6a921a8ef42b2907d27f42e7ac6e
Jereamy Smothers <jereamyasmothers@gmail.com>
2022-10-07 19:28:02 -0700

    added in models folder with files and finished index.js got models

---
+++ src/models/index.js
@@ -0,0 +1,6 @@
+const Game = require('./game');
+const Player = require('./player');
+const Enemy = require('./enemy');
+const Items = require('./items');
+
+module.exports = { Game, Player, Enemy, Items };
