64b4917297f93fc5ac28c0bafcf4b5ec1c73d218
loudagnese <louis.dagnese@gmail.com>
2022-10-08 09:52:05 -0700

    code from class and added filler picture

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useState } from "react";
 import apple from './assets/apple.png';
 import glowingApple from './assets/glowingapple.png';
@@ -96,4 +97,44 @@ const InteractObject = ({ image, glowyImage, location }) => {
 useKeyPress();
 
 
+=======
+import { useEffect, useRef, useState } from "react";
+import char from './mario.png';
+
+const App = () => {
+    const controlsRef = useRef();
+
+    useEffect(() => {
+        controlsRef.current.focus();
+        console.log(controlsRef.current);
+    }, []);
+
+    const initialCharState = {
+        image: char,
+        x: 500,
+        y: 300
+    }
+    const [character, updateCharacter] = useState(initialCharState);
+
+    const controls = (e) => {
+        console.log(e);
+        if(e.key === 'd') {
+            updateCharacter({...character, x: character.x + 4})
+        }
+        if(e.key === 'a') {
+          updateCharacter({...character, x:character.x - 4})
+        }
+
+    }
+
+    return (
+        <div tabIndex={0} onKeyDown={controls} ref={controlsRef} style={{width: '1000px', height: '1000px'}}  >
+            <div  style={{ position: "absolute", top: `${character.y}px`, left: `${character.x}px` }}>
+                <img alt="character" src={character.image} />
+            </div>
+        </div>
+    )
+}
+
+>>>>>>> Stashed changes
 export default App;
