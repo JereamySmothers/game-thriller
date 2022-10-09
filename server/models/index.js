865ee4870909022990b4ff3d31784cfc04a4316c
loudagnese <louis.dagnese@gmail.com>
2022-10-09 14:38:20 -0700

    fix develop branch with front end and back end, finish login and auth

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-const Player = require('./player');
-const Enemy = require('./enemy');
-const Potion = require('./potion');
-
-module.exports = { Player, Enemy, Potion };
-=======
 const Player = require("./Player");
 const Score = require("./Score");
 
 module.exports = { Player, Score };
->>>>>>> feature/auth
