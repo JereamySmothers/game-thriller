d1522f6faacd8427a41ba9b9d60d82a92ef4c5ef
Jereamy Smothers <jereamyasmothers@gmail.com>
2022-10-09 16:48:56 -0700

    merged and got basics of highscore page; first commit

---
+++ client/src/pages/score.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { playerName, Score } from "../../../server/models/Player";
+
+const Highscore = () => {
+
+    return (
+        <div>
+            <Header>Highscores</Header>
+
+                <ol>
+                    <li>
+                        
+                    </li>
+                </ol>
+        </div>
+    );
+};
+
+export default Highscore
