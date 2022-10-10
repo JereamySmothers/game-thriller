b3990dd7f9f9197b6247f4e0eda507fe8236ba0b
EZaiger <elisabeth.zaiger@oit.edu>
2022-10-09 22:09:30 -0700

    imported code from module assignment to start connecting frontend to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 
-
 import { useEffect, useRef, useState } from "react";
-import char from './mario.png';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+  } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
     const controlsRef = useRef();
