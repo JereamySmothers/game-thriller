3a58d828050470875e079858ddddd80b4767bcc6
janethxuiz <janethruiz152@gmail.com>
2022-10-11 17:21:23 -0700

    formatted signup and login with prettier

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="login">
+      <div className="login-container">
+        <h1>Please Login to continue</h1>
+        <label for="email">
+          <b>Email</b>
+        </label>
+        <input type="text" placeholder="Enter Email" name="email" required />
+        <label for="psw">
+          <b>Password</b>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="psw"
+          required
+        />
+        <button type="submit">Submit</button>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
