fa691b59e5d2d6a7eab9d15a8d998176505a98f8
EZaiger <elisabeth.zaiger@oit.edu>
2022-10-16 21:50:49 -0700

    created build version of app

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/octocat-game", {});
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/octocat-game", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
