09904c33dba75a1d50d02617cdeed7cdbdbf934f
EZaiger <elisabeth.zaiger@oit.edu>
2022-10-16 21:25:24 -0700

    commented out code

--- server/server.js
+++ server/server.js
@@ -18,9 +18,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
