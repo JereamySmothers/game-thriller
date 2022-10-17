7e40b5579157bcbf03f3c2ae14b03f1fb13f5d62
EZaiger <elisabeth.zaiger@oit.edu>
2022-10-16 21:09:31 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/octocat-game", {});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/octocat-game", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
