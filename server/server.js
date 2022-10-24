360a3d4d6e61b30405055ed5a1a527abca70c58b
EZaiger <elisabeth.zaiger@oit.edu>
2022-10-23 19:29:03 -0700

    server.js path addition

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
